2d810a771e4ab1a4a7b5dc3f79c60f13d337e274
Daniel Noon <supercookies@outlook.com>
2015-07-12 15:54:04 -0500

    began update function, added more to player

--- game.js
+++ game.js
@@ -1,15 +1,22 @@
 /// <reference path="typings/jquery/jquery.d.ts"/>
 window.setTimeout(function(){
-document.querySelector("#hp>.progressbar").className += " mdl-color--blue";
-document.querySelector("#xp>.progressbar").className += " mdl-color--teal";
+    document.querySelector("#hp>.progressbar").className += " mdl-color--blue";
+    document.querySelector("#xp>.progressbar").className += " mdl-color--teal";
+    update("start");
 }, 1000);
 
 var stories;
 
-
 var hp = document.querySelector("#hp");
 var xp = document.querySelector("#xp");
 var mana = document.querySelector("#mana");
+var str = $("#str");
+var dex = $("#dex");
+var cha = $("#cha");
+var con = $("#con");
+var int = $("#int");
+var wis = $("#wis");
+
 
 $.getJSON("stories.json", function(data){
     stories = data;
@@ -18,3 +25,35 @@ $.getJSON("stories.json", function(data){
 function fabToggle() {
     $("#fabNext").slideToggle();
 }
+
+var player = {
+    str: 10,
+    dex: 10,
+    con: 10,
+    int: 10,
+    wis: 10,
+    cha: 10,
+    hp: 100,
+    maxHp: 100,
+    xp: 0,
+    nextLevel: 20,
+    mana: 100,
+    maxMana: 100,
+    level: 0,
+};
+
+function update(why) {
+    str.text(player.str);
+    dex.text(player.dex);
+    con.text(player.con);
+    int.text(player.int);
+    cha.text(player.cha);
+    wis.text(player.wis);
+    hp.MaterialProgress.setProgress((player.hp/player.maxHp) * 100);
+    xp.MaterialProgress.setProgress((player.xp/player.nextLevel) * 100);
+    mana.MaterialProgress.setProgress((player.mana/player.maxMana) * 100);
+};
+
+Object.observe(player, function (changes) {
+    update("update");
+});
