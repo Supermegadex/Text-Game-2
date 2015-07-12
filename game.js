84d94ba4250fb002c63d110d761e626a6d5df04a
Daniel Noon <supercookies@outlook.com>
2015-07-11 19:51:29 -0500

    Absolutely nothing new for end-users--but added a few background functions

--- game.js
+++ game.js
@@ -1,4 +1,15 @@
+/// <reference path="typings/jquery/jquery.d.ts"/>
+window.setTimeout(function(){
+document.querySelector("#hp>.progressbar").className += " mdl-color--blue";
+document.querySelector("#xp>.progressbar").className += " mdl-color--teal";
+}, 1000);
+
 var stories;
+
+
+var hp = document.querySelector("#hp");
+var xp = document.querySelector("#xp");
+var mana = document.querySelector("#mana");
 
 $.getJSON("stories.json", function(data){
     stories = data;
