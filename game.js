db6d16cbdcb8879982bc6f33b051dbf92ca47be3
Daniel Noon <supercookies@outlook.com>
2015-07-13 19:10:08 -0500

    Working navigation between attributes with buttons and a fab

--- game.js
+++ game.js
@@ -1,22 +1,15 @@
 /// <reference path="typings/jquery/jquery.d.ts"/>
 window.setTimeout(function(){
-    document.querySelector("#hp>.progressbar").className += " mdl-color--blue";
-    document.querySelector("#xp>.progressbar").className += " mdl-color--teal";
-    update("start");
+document.querySelector("#hp>.progressbar").className += " mdl-color--blue";
+document.querySelector("#xp>.progressbar").className += " mdl-color--teal";
 }, 1000);
 
 var stories;
 
+
 var hp = document.querySelector("#hp");
 var xp = document.querySelector("#xp");
 var mana = document.querySelector("#mana");
-var str = $("#str");
-var dex = $("#dex");
-var cha = $("#cha");
-var con = $("#con");
-var int = $("#int");
-var wis = $("#wis");
-
 
 $.getJSON("stories.json", function(data){
     stories = data;
@@ -25,35 +18,3 @@ $.getJSON("stories.json", function(data){
 function fabToggle() {
     $("#fabNext").slideToggle();
 }
-
-var player = {
-    str: 10,
-    dex: 10,
-    con: 10,
-    int: 10,
-    wis: 10,
-    cha: 10,
-    hp: 100,
-    maxHp: 100,
-    xp: 0,
-    nextLevel: 20,
-    mana: 100,
-    maxMana: 100,
-    level: 0,
-};
-
-function update(why) {
-    str.text(player.str);
-    dex.text(player.dex);
-    con.text(player.con);
-    int.text(player.int);
-    cha.text(player.cha);
-    wis.text(player.wis);
-    hp.MaterialProgress.setProgress((player.hp/player.maxHp) * 100);
-    xp.MaterialProgress.setProgress((player.xp/player.nextLevel) * 100);
-    mana.MaterialProgress.setProgress((player.mana/player.maxMana) * 100);
-};
-
-Object.observe(player, function (changes) {
-    update("update");
-});
